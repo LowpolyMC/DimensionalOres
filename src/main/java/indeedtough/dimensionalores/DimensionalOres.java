0bd467a804b4935bb23c4f999c4053c8c89c1448
AcePolitician <vicu.ciobu@gmail.com>
2024-07-31 13:05:29 -0400

    Initial commit

--- src/main/java/indeedtough/dimensionalores/DimensionalOres.java
+++ src/main/java/indeedtough/dimensionalores/DimensionalOres.java
@@ -4,7 +4,6 @@ import indeedtough.dimensionalores.block.ModBlocks;
 import indeedtough.dimensionalores.item.ModItemGroups;
 import indeedtough.dimensionalores.world.gen.WorldGeneration;
 import net.fabricmc.api.ModInitializer;
-import net.minecraft.client.MinecraftClient;
 
 public class DimensionalOres implements ModInitializer {
 	public static final String MOD_ID = "dimensionalores";
